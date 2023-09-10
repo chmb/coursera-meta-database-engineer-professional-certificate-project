7d0a401ef274694a2bbd27acfbd9caee000f7ed5
Chomba Waihenya <chmb@users.noreply.github.com>
2023-09-10 08:39:47 +0300

    changes made to meet requirements

--- add_booking_query.sql
+++ add_booking_query.sql
@@ -4,6 +4,5 @@ CREATE PROCEDURE AddBooking(Booking_Slot DATETIME, Table_Number INT, Customer_Id
 BEGIN
 INSERT INTO bookings (BookingSlot,TableNo,Customers_CustomerID)
 VALUES(Booking_Slot, Table_Number, Customer_Id);
-SELECT 'New booking added' AS 'Confirmation';
 END //
 DELIMITER ;
--- cancel_booking_query.sql
+++ cancel_booking_query.sql
@@ -3,6 +3,5 @@ DELIMITER //
 CREATE PROCEDURE CancelBooking(Booking_Id INT)
 BEGIN
 DELETE FROM bookings WHERE BookingID = Booking_Id;
-SELECT CONCAT('Booking ', booking_id, ' cancelled') AS 'Confirmation';
 END //
 DELIMITER ;
